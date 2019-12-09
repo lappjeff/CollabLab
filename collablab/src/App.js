f0faccd6cb286b49b78fc18bf3391c57b2402d4e
Jeff Lapp <lappjeff20@gmail.com>
2019-12-09 17:03:19 -0500

    Set up login button view
    - Set up landing page
    - Set up login button for spotify
    - Styled button to center on page

--- collablab/src/App.js
+++ collablab/src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import LandingPage from "./components/LandingPage";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<LandingPage />
+		</div>
+	);
 }
 
 export default App;
--- collablab/src/components/LandingPage.js
+++ collablab/src/components/LandingPage.js
@@ -1,1 +1,16 @@
 import React from "react";
+import { Container, Row, Col, Button } from "reactstrap";
+import "../styles/landingpage.scss";
+const LandingPage = () => {
+	return (
+		<Container fluid className="landing-page">
+			<Row className="h-100 align-items-center">
+				<Col className="text-center">
+					<Button>Log In With Spotify</Button>
+				</Col>
+			</Row>
+		</Container>
+	);
+};
+
+export default LandingPage;
