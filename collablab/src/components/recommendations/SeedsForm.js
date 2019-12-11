bbdb003435e861f6bb14adb949125309640245a5
Jeff Lapp <lappjeff20@gmail.com>
2019-12-10 19:06:17 -0500

    Started work on sliders for values

--- collablab/src/components/recommendations/SeedsForm.js
+++ collablab/src/components/recommendations/SeedsForm.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Container, FormGroup, Label, CustomInput } from "reactstrap";
 
 const SeedsForm = () => {
-	return <h1>SeedsForm</h1>;
+	return (
+		<Container className="d-flex justify-content-center">
+			<FormGroup className="w-50 m-3">
+				<Label for="acousticness">Acousticness</Label>
+				<CustomInput
+					type="range"
+					id="acousticness"
+					name="acousticness slider"
+					onChange={e => console.log(e.target.value)}
+				/>
+			</FormGroup>
+		</Container>
+	);
 };
 
 export default SeedsForm;
