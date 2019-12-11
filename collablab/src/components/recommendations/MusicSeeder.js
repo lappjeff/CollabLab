aee1bbbe2b7101bfa1195ad6fbcf19791eced221
Jeff Lapp <lappjeff20@gmail.com>
2019-12-11 16:54:43 -0500

    Created MusicSeeder component

---
+++ collablab/src/components/recommendations/MusicSeeder.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Col } from "reactstrap";
+
+const MusicSeeder = () => {
+	return (
+		<Col>
+			<h1>MusicSeeder</h1>
+		</Col>
+	);
+};
+
+export default MusicSeeder;
