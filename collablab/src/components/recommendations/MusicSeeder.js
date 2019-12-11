1faff71c210c1cea8a5cbff91e85e88300b74c25
Jeff Lapp <lappjeff20@gmail.com>
2019-12-11 17:34:49 -0500

    Added base song search logic to MusicSeeder component

--- collablab/src/components/recommendations/MusicSeeder.js
+++ collablab/src/components/recommendations/MusicSeeder.js
@@ -1,12 +1,39 @@
+import cookie from "js-cookie";
 import React, { useState } from "react";
-import { Col, Input } from "reactstrap";
+import { Col, Input, Button } from "reactstrap";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const accessToken = cookie.get("spotifyAccessToken");
+const spotify = new SpotifyWebApi();
+spotify.setAccessToken(accessToken);
 
 const MusicSeeder = () => {
 	const [searchInput, setSearchInput] = useState("");
+
+	const searchSong = async name => {
+		try {
+			const track = await spotify.searchTracks(name, { limit: 1 });
+		} catch (err) {
+			console.log(err);
+		}
+	};
 	return (
 		<Col>
 			<h1>MusicSeeder</h1>
-			<Input value={searchInput} placeholder="Search song"></Input>
+			<Input
+				value={searchInput}
+				placeholder="Search song"
+				onChange={e => {
+					setSearchInput(e.target.value);
+				}}
+			></Input>
+			<Button
+				onClick={e => {
+					searchSong(searchInput);
+				}}
+			>
+				Search Song
+			</Button>
 		</Col>
 	);
 };
