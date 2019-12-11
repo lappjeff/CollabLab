fe58d879a39a34df54eaf7dd2e9a9cc790aba7af
Jeff Lapp <lappjeff20@gmail.com>
2019-12-11 17:02:40 -0500

    Built out input for searching songs in MusicSeeder

--- collablab/src/components/recommendations/MusicSeeder.js
+++ collablab/src/components/recommendations/MusicSeeder.js
@@ -1,10 +1,12 @@
-import React from "react";
-import { Col } from "reactstrap";
+import React, { useState } from "react";
+import { Col, Input } from "reactstrap";
 
 const MusicSeeder = () => {
+	const [searchInput, setSearchInput] = useState("");
 	return (
 		<Col>
 			<h1>MusicSeeder</h1>
+			<Input value={searchInput} placeholder="Search song"></Input>
 		</Col>
 	);
 };
