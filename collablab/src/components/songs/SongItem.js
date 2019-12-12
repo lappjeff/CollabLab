f02a6f7e0f2d8fe946cb1edba8a2ff4ff2c72783
Jeff Lapp <lappjeff20@gmail.com>
2019-12-11 19:49:26 -0500

    Changed layout to reactstrap Toast component

--- collablab/src/components/songs/SongItem.js
+++ collablab/src/components/songs/SongItem.js
@@ -1,15 +1,14 @@
 import React from "react";
-import { Row, Col } from "reactstrap";
+import { Row, Toast, ToastHeader } from "reactstrap";
 
 const SongItem = ({ song }) => {
 	return (
-		<Row className="m-1">
-			<Col>
-				<img src={song.album.images[2].url}></img>
-			</Col>
-			<Col>
-				<p>{song.name}</p>
-			</Col>
+		<Row className="m-1 w-70 align-items-center">
+			<Toast>
+				<ToastHeader icon={<img src={song.album.images[2].url}></img>}>
+					{song.name} -{song.artists[0].name}
+				</ToastHeader>
+			</Toast>
 		</Row>
 	);
 };
