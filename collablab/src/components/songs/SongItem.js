26af63e07af68f7ce27b9bdec3196329f7f608f1
Jeff Lapp <lappjeff20@gmail.com>
2019-12-11 19:14:08 -0500

    Changed components involved in SongItem markup

--- collablab/src/components/songs/SongItem.js
+++ collablab/src/components/songs/SongItem.js
@@ -1,23 +1,15 @@
 import React from "react";
-import {
-	Card,
-	CardImg,
-	CardText,
-	CardBody,
-	CardTitle,
-	CardSubtitle,
-	Button,
-	Row,
-	Col
-} from "reactstrap";
+import { Row, Col } from "reactstrap";
 
 const SongItem = ({ song }) => {
-	console.log(song);
 	return (
 		<Row className="m-1">
-			<Card>
-				<CardImg src={song.album.images[2].url} alt="song album cover" />
-			</Card>
+			<Col>
+				<img src={song.album.images[2].url}></img>
+			</Col>
+			<Col>
+				<p>{song.name}</p>
+			</Col>
 		</Row>
 	);
 };
