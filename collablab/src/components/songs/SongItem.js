c947aa09af642ecd72325328427de26c87b1eb24
Jeff Lapp <lappjeff20@gmail.com>
2019-12-11 19:04:18 -0500

    Added some display markup to SongItem

--- collablab/src/components/songs/SongItem.js
+++ collablab/src/components/songs/SongItem.js
@@ -1,7 +1,25 @@
 import React from "react";
+import {
+	Card,
+	CardImg,
+	CardText,
+	CardBody,
+	CardTitle,
+	CardSubtitle,
+	Button,
+	Row,
+	Col
+} from "reactstrap";
 
 const SongItem = ({ song }) => {
-	return <p>Songitem</p>;
+	console.log(song);
+	return (
+		<Row className="m-1">
+			<Card>
+				<CardImg src={song.album.images[2].url} alt="song album cover" />
+			</Card>
+		</Row>
+	);
 };
 
 export default SongItem;
